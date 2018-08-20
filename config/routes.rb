ad98b586916c8e34b3f5084686ba0715a9fb8b9b
Kay Mok <kaymok3@gmail.com>
2018-08-20 16:40:04 -0400

    Update routes naming and specify actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :memes
-  resources :searches
+  resources :faves, only: [:index, :create, :edit], controller: 'memes'
+  resources :results, only: [:index, :create], controller: 'searches'
   root 'searches#index'
 end
